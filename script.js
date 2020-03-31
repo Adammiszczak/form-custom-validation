c6ce9f7244cec263cc1a9cea091d27a59bfc1797
Adam Miszczak <adammiszczak1@gmail.com>
2020-03-31 23:32:33 +0200

    Prepare regexs, listener for further develop with one test error

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 // Selected elements
 
 let mainForm = document.querySelector("form#mainForm");
+let formInputs = [...document.querySelectorAll("input")];
 let formName = document.querySelector("input#formName");
 let formSurname = document.querySelector("input#formSurname");
 let formEmail = document.querySelector("input#formEmail");
 let formImage = document.querySelector("input#formImage");
 let formMessage = document.querySelector("input#formMessage");
 
+//Regular expresions
+
+let nameRegex = /[a-z]{2,}/i;
+let surnameRegex = /[a-z]{3,}/i;
+let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+let imageRegex = /(\.jpg|\.png)$/;
 
 // validation 
 
 mainForm.addEventListener("submit", (event) => {
-        event.preventDefault();
-        console.log("Form submitted");
+        if (nameRegex.test(formName.value)) {
+            return true;
+        } else {
+            event.preventDefault();
+            console.log("Form not submitted");
+            
+        }
+
 })
+
